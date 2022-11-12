26f539ecb1733ffbf421fe53b3c4575efa4ffe74
Kaiyang Wu <kaiyaw@uw.edu>
2022-11-12 14:44:44 -0800

    source/chart1: update chart

--- source/chart1.R
+++ source/chart1.R
@@ -5,6 +5,7 @@ chart1 <- function() {
   
   title <- "Total Greenhouse Gas Emissions in the US"
   subtitle <- "Gas Emissions by Gas Type"
+  caption <- "* HFCs are hydrofluorocarbons, PFCs are perfluorocarbons, SF6 is sulfur hexafluoride, and NF3 is nitrogen trifluoride."
   
   data <- read.csv("../data/us-ghg-emissions_fig-1.csv") %>%
     gather(key = Gas.Type, value = Emission, -Year) %>%
@@ -17,9 +18,10 @@ chart1 <- function() {
     geom_col(mapping = aes(x = "", y = Total.Emission, fill = Gas.Type)) +
     labs(
       x = "",
-      y = "",
+      y = "Emissions (million metric tons of carbon dioxide equivalents)",
       title = title,
-      subtitle = subtitle
+      subtitle = subtitle,
+      caption = caption
     )
 
   return(chart)
