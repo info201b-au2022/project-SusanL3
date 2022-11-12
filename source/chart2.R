452bd56dc89c9ddeab9de99351f8df89a5e0f1ac
mileszhanghao <hzhang44@uw.edu>
2022-11-12 14:39:39 -0800

    chart2: finish

--- source/chart2.R
+++ source/chart2.R
@@ -3,8 +3,8 @@ chart2 <- function() {
   library(ggplot2)
   library(hrbrthemes)
   library(tidyverse)
-  title  <- "annual average temperature anomaly"
-  subtitle <- "ClimDiv with USCRN"
+  title  <- "U.S. Climate Extremes Index (CEI)"
+  subtitle <- "Data Source: ClimDiv"
   
   anomaly <- read.csv("../data/annual_average_temperature_anomaly.csv", stringsAsFactors = FALSE)
 
@@ -16,8 +16,18 @@ chart2 <- function() {
   
   # Most basic error bar
   p <- ggplot(data) +
-    geom_bar(mapping = aes(x=date, y=value), stat="identity", fill="green", size = 30, alpha=0.7) +
-    geom_smooth(mapping = aes(x=date, y=value))
+    geom_bar(mapping = aes(x=date, y=value), stat="identity", fill="red", width = 5, alpha=0.7) +
+    geom_smooth(mapping = aes(x=date, y=value)) +
+    labs(
+      x = "Date",
+      y = "Climate Extremes Index (CEI)",
+      title = title,
+      subtitle = subtitle
+    )
   
   return(p)
 }
+
+# the bar plot with smooth is about the relationship between the climate extremes index (CEI) and date.
+# in the smooth line, it shows the CEI be positive from negative value as time goes. 
+# it means the climate is becoming more and more extremely near the several decades because there are more positive bars than in the past.
